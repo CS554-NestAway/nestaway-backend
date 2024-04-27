626f4a20d4c476351b54b965f43ed83ee97765c9
bobwen0205 <wenmingzhi10@gmail.com>
2024-04-27 00:42:46 -0400

    Update helper.js

--- helper.js
+++ helper.js
@@ -10,3 +10,10 @@ export const getMongoID = (id) => {
     throw "Invalid ID provided";
   }
 };
+
+export const computeDaysByCheckInAndCheckOut = (checkIn, checkOut) => {
+  const differenceInTime = checkOut.getTime() - checkIn.getTime();
+  const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+  const numberOfDays = Math.round(differenceInDays);
+  return numberOfDays;
+}
