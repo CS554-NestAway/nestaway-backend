92335ecbe9d7afeee8bf6f726eed998d2a41a642
bobwen0205 <wenmingzhi10@gmail.com>
2024-05-08 16:09:58 -0400

    Update booking.js

--- routes/booking.js
+++ routes/booking.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import * as validation from '../validation/validateBooking.js';
-import { getHouseById, getUserById, addBookingByHouseId } from '../data/booking.js';
+import { getHouseById, getUserById, addBookingByHouseId, getAllBookingsByUserId, toggleBookingStatus } from '../data/booking.js';
 import {
     checkIfLoggedIn,
     checkIfHouseBelongsToHost,
@@ -38,6 +38,25 @@ router.post('/addBooking', checkIfLoggedIn, async (req, res) => {
         return res.status(400).json({ error: e });
     }
     return res.status(200).json({ updatedHouse: updatedHouse, success: true });
+});
+
+router.get('/bookings', checkIfLoggedIn, async (req, res) => {
+    try {
+        const bookingArray = await getAllBookingsByUserId(req.user.uid);
+        return res.status(200).json({ data: bookingArray });
+    } catch (e) {
+        return res.status(400).json({ error: e });
+    }
+});
+
+router.post('/toggleStatus', checkIfLoggedIn, async (req, res) => {
+    try {
+        const { houseId, bookingId, status } = req.body;
+        const updatedBooking = await toggleBookingStatus(houseId, bookingId, status);
+        return res.status(200).json({ updatedBooking: updatedBooking, success: true });
+    } catch (e) {
+        return res.status(400).json({ error: e });
+    }
 });
 
 export default router;
